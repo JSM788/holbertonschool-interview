a5f8417a9749790e597a704a9d524b923107c8db
JSM788 <salinasjuan788@gmail.com>
2022-12-09 12:44:08 -0500

    Adding 0-lockboxes.py file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining my function canUnlockAll"""
+
+
+def canUnlockAll(boxes):
+    """This function determines if all the boxes can be opened"""
+    keys = [0]
+    size = len(boxes)
+
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys:
+                keys.append(box)
+    if size == len(keys):
+        return True
+    return False
