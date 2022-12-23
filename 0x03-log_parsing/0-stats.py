39d47eb3dcf0a5d57aa794e8c644a1b6cc9f280f
JSM788 <salinasjuan788@gmail.com>
2022-12-22 23:47:29 -0500

    Adding 0-stats.py file

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This script reads stdin line by line computes metrics"""
+import sys
+
+status_codes = {}
+total_size = 0
+count = 0
+
+for line in sys.stdin:
+    words = line.split(" ")
+    if len(words) != 9:
+        continue
+
+    count += 1
+    total_size += int(words[8])
+
+    status_code = words[7]
+    if status_code in status_codes:
+        status_codes[status_code] += 1
+    else:
+        status_codes[status_code] = 1
+
+    if count % 10 == 0:
+        print("File size: {}".format(total_size))
+        for status_code, count in sorted(status_codes.items()):
+            print("{}: {}".format(status_code, count))
+        status_codes = {}
